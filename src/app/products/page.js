c356517ddc62d01c5c4eace338272258c2d1c509
krishnatejayadavanangi <akrishtej@gmail.com>
2026-04-28 18:15:16 +0530

    first commit

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,12 +1,18 @@
 import MainLayout from '@/components/layout/MainLayout';
 import ProductsClient from './ProductsClient';
+import { Suspense } from 'react';
 
-export const metadata = { title: 'All Products', description: 'Browse our complete range of baby & kids products' };
+export const metadata = {
+  title: 'All Products',
+  description: 'Browse our complete range of baby & kids products'
+};
 
 export default function ProductsPage() {
   return (
     <MainLayout>
-      <ProductsClient />
+      <Suspense fallback={<div>Loading products...</div>}>
+        <ProductsClient />
+      </Suspense>
     </MainLayout>
   );
 }
